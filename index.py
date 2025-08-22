1413d8000ac8a356a927e8b789ff6bfba28451ee
unknown <aamirsubair19@gmail.com>
2025-08-22 22:54:21 +0530

    Initial commit

---
+++ index.py
@@ -0,0 +1,14 @@
+# Simple math operations in Python
+
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
